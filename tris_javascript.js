b0a4cf55758bc0b676f108dca365962715a57d85
Sebastiano <la-tua-email@esempio.com>
2026-04-14 15:51:31 +0200

    commit

--- tris_javascript.js
+++ tris_javascript.js
@@ -1,5 +1,21 @@
-let tasto=document.getElementById("bottone");
+let tasto=document.querySelectorAll(".bottone");
+let vis_x=document.querySelectorAll(".visualizzazione_x");
+let vis_o=document.querySelectorAll(".visualizzazione_o");
+let cont=0;
 
-tasto.addEventListener("click", function(){
-    document.getElementById("Visualizzazione").textContent="x";
-});
+for(let i=0;i<tasto.length;i++)
+{
+    tasto[i].addEventListener("click",function(){
+        if(cont %2===0)
+        {
+                vis_x[i].textContent="x";
+                tasto[i].disabled = true; 
+        }
+        else
+        {
+                vis_o[i].textContent="o";
+                tasto[i].disabled = true; 
+        }
+        cont++;    
+    });
+}
