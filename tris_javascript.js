e2b9349ab150752074634f332c54ccc8b67fd57f
Sebastiano <la-tua-email@esempio.com>
2026-04-20 17:39:13 +0200

    commit

--- tris_javascript.js
+++ tris_javascript.js
@@ -1,6 +1,6 @@
 // funzioni schermo condiviso
 
-function Verifica_vittoria_x(controllo_x,tasto,vis_x,vis_x)
+function Verifica_vittoria_x(controllo_x,tasto,vis_x)
 {
     if(controllo_x[0]=="x" && controllo_x[1]=="x" && controllo_x[2]=="x")
     {
@@ -126,7 +126,7 @@ function Verifica_vittoria_x(controllo_x,tasto,vis_x,vis_x)
     return 0;
 }
 
-function Verifica_vittoria_o(controllo_o,tasto,vis_o,vis_o)
+function Verifica_vittoria_o(controllo_o,tasto,vis_o)
 {
     if(controllo_o[0]=="o" && controllo_o[1]=="o" && controllo_o[2]=="o")
     {
@@ -260,6 +260,7 @@ let gioco_2=document.getElementById("gioco2");
 let scelta_1=document.getElementById("scelta1");
 let scelta_2=document.getElementById("scelta2");
 let titolo_sc=document.getElementById("titolo_sc");
+let titolo_gc=document.getElementById("titolo_gc");
 
 scelta_1.addEventListener("click",function(){
     menù.style.display = "none";
@@ -299,37 +300,37 @@ scelta_1.addEventListener("click",function(){
     //-------------------------------------------------------------------------------------
     // funzione reset
     function ResetPartita()
-{
-    vittoria_x=false;
-    vittoria_o=false;
-    cont=0;
-
-    for(let j=0;j<9;j++)
     {
-        vis_x[j].textContent="";
-        vis_o[j].textContent="";
+        vittoria_x=false;
+        vittoria_o=false;
+        cont=0;
 
-        controllo_x[j]="";
-        controllo_o[j]="";
-        tasto[j].disabled = false;
+        for(let j=0;j<9;j++)
+        {
+            vis_x[j].textContent="";
+            vis_o[j].textContent="";
 
-        tasto[j].style.backgroundColor = "";
-        vis_x[j].style.backgroundColor = "";
-        vis_o[j].style.backgroundColor = "";
+            controllo_x[j]="";
+            controllo_o[j]="";
+            tasto[j].disabled = false;
 
-        vis_x[j].style.color = "";
-        vis_o[j].style.color = "";
+            tasto[j].style.backgroundColor = "";
+            vis_x[j].style.backgroundColor = "";
+            vis_o[j].style.backgroundColor = "";
 
-        tasto[j].style.border = "";
+            vis_x[j].style.color = "";
+            vis_o[j].style.color = "";
 
-        tasto[j].style.padding = "";
-        vis_x[j].style.padding = "";
-        vis_o[j].style.padding = "";
+            tasto[j].style.border = "";
+
+            tasto[j].style.padding = "";
+            vis_x[j].style.padding = "";
+            vis_o[j].style.padding = "";
+        }
+
+        h1_sc[0].textContent = "Turno X";
+        h1_sc[0].style.color = "blue";
     }
-
-    h1_sc[0].textContent = "Turno X";
-    h1_sc[0].style.color = "blue";
-}
 
     //------------------------------------------------------------
 
@@ -366,7 +367,7 @@ scelta_1.addEventListener("click",function(){
                 vis_o[i].style.color="red";
                 tasto[i].disabled = true;
                 controllo_o[i] = "o"; 
-                if(Verifica_vittoria_o(controllo_o,tasto,vis_o,vis_o)==2)
+                if(Verifica_vittoria_o(controllo_o,tasto,vis_o)==2)
                 {
                     vittoria_o=true;
                     cont_sect2_3_sc++;
@@ -379,27 +380,16 @@ scelta_1.addEventListener("click",function(){
             cont++;    
             if(n==0)
             {
-                if(cont % 2 === 0){
-                    h1_sc[0].textContent = "Turno X";
-                    h1_sc[0].style.color="blue";
-                }           
-                else 
+                if(
+                    tasto[0].disabled && tasto[1].disabled && tasto[2].disabled &&
+                    tasto[3].disabled && tasto[4].disabled && tasto[5].disabled &&
+                    tasto[6].disabled && tasto[7].disabled && tasto[8].disabled
+                )
                 {
-                    h1_sc[0].textContent = "Turno O";
-                    h1_sc[0].style.color="red";
-                }
-                if(tasto[0].disabled === true && tasto[1].disabled === true && tasto[2].disabled === true && tasto[3].disabled === true && tasto[4].disabled === true && tasto[5].disabled === true && tasto[6].disabled === true && tasto[7].disabled === true && tasto[8].disabled === true)
-                {
-                    cont_sect2_2_sc++;
-                    sect2_2_sc.textContent="Pareggi:"+ cont_sect2_2_sc;
-                    if(vittoria_x!=true)
-                    {
-                        return;
-                    }  
-                    if(vittoria_o!=true)
-                    {
-                        return;
-                    }
+                    h1_sc[0].textContent = "Pareggio";
+                    h1_sc[0].style.color = "black";
+                    cont_sect2_2_sc++; 
+                    sect2_2_sc.textContent = "Pareggi: " + cont_sect2_2_sc;
                 }
             }
             else
@@ -419,15 +409,25 @@ scelta_1.addEventListener("click",function(){
 scelta_2.addEventListener("click",function(){
     menù.style.display = "none";
     gioco_2.style.display = "block";
+    titolo_gc.style.display = "block";
 
     // ------------------------------------------------------------------------------------
     // gioco contro il computer
     
+    let h1_gc=document.querySelectorAll(".h1_gc");
+    let sect2_1_gc=document.querySelectorAll(".sect2_1_gc")[0];
+    let sect2_2_gc=document.querySelectorAll(".sect2_2_gc")[0];
+    let sect2_3_gc=document.querySelectorAll(".sect2_3_gc")[0];
+    let cont_sect2_1_gc=0;
+    let cont_sect2_2_gc=0;
+    let cont_sect2_3_gc=0;
+
     let tasto_cpu=document.querySelectorAll(".bottone_cpu");
     let vis_x_cpu=document.querySelectorAll(".visualizzazione_x_cpu");
     let vis_o_cpu=document.querySelectorAll(".visualizzazione_o_cpu");
     let vit = document.getElementById("vittoria");
     let ncasuale=0;
+    let n_cpu=0;
     let n=0;
     let controllocasuale=[0,0,0,0,0,0,0,0,0];
     let vittoria_x_cpu=false;
@@ -440,8 +440,52 @@ scelta_2.addEventListener("click",function(){
                         "","","",
                         "","",""
                         ];
+    
+    let reset_gc= document.getElementById("reset_gc");
+    let home_gc= document.getElementById("home_gc");
+
+    //-------------------------------------------------------------------------------------
+    // funzione reset
+
+    function ResetPartita_gc()
+    {
+        vittoria_x_cpu = false;
+        vittoria_o_cpu = false;
+
+        n_cpu = 0;
+        n = 0;
+
+        for(let j=0; j<9; j++)
+        {
+            controllocasuale[j] = 0;
+
+            vis_x_cpu[j].textContent = "";
+            vis_o_cpu[j].textContent = "";
+
+            controllo_x_cpu[j] = "";
+            controllo_o_cpu[j] = "";
+
+            tasto_cpu[j].disabled = false;
+
+            tasto_cpu[j].style.backgroundColor = "";
+            vis_x_cpu[j].style.backgroundColor = "";
+            vis_o_cpu[j].style.backgroundColor = "";
+
+            vis_x_cpu[j].style.color = "";
+            vis_o_cpu[j].style.color = "";
+
+            tasto_cpu[j].style.border = "";
+            tasto_cpu[j].style.padding = "";
+            vis_x_cpu[j].style.padding = "";
+            vis_o_cpu[j].style.padding = "";
+        }
+
+        h1_gc[0].textContent = "Turno X";
+        h1_gc[0].style.color = "blue";
+    }
 
 
+    //------------------------------------------------------------
     for(let i=0;i<tasto_cpu.length;i++)
     {
         tasto_cpu[i].addEventListener("click",function(){
@@ -450,15 +494,36 @@ scelta_2.addEventListener("click",function(){
                 return;
             }
             vis_x_cpu[i].textContent="x";
+            vis_x_cpu[i].style.color="blue";
             tasto_cpu[i].disabled = true;
             controllocasuale[i]=1; 
             controllo_x_cpu[i]="x";
-            if(Verifica_vittoria_x(controllo_x_cpu)==1)
+            if(Verifica_vittoria_x(controllo_x_cpu,tasto_cpu,vis_x_cpu)==1)
             {
-                vit.textContent="vittoria x";
                 vittoria_x_cpu=true;
+                cont_sect2_1_gc++;
+                h1_gc[0].textContent="Vittoria X";
+                h1_gc[0].style.color="blue";
+                sect2_1_gc.textContent="Giocatore 1(X): "+ cont_sect2_1_gc;
+                n_cpu++;
                 return;
             }
+
+            let caselle_occupate = 0;
+            for (let j = 0; j < 9; j++) {
+                if (tasto_cpu[j].disabled == true) {
+                    caselle_occupate++;
+                }
+            }
+            if (caselle_occupate == 9 && vittoria_x_cpu == false) {
+                h1_gc[0].textContent = "Pareggio";
+                h1_gc[0].style.color = "black";
+                
+                cont_sect2_2_gc++;
+                sect2_2_gc.textContent = "Pareggi: " + cont_sect2_2_gc;
+                return;
+            }
+
             do{
                 n=0;
                 ncasuale=Math.floor(Math.random()*9);
@@ -468,15 +533,45 @@ scelta_2.addEventListener("click",function(){
                 }
             }while(n==1);
             vis_o_cpu[ncasuale].textContent="o";
+            vis_o_cpu[ncasuale].style.color="red";
             tasto_cpu[ncasuale].disabled = true;
             controllo_o_cpu[ncasuale] = "o"; 
             controllocasuale[ncasuale]=1;
-            if(Verifica_vittoria_o(controllo_o_cpu)==2)
+            if(Verifica_vittoria_o(controllo_o_cpu,tasto_cpu,vis_o_cpu)==2)
             {
-                vit.textContent="vittoria o";
                 vittoria_o_cpu=true;
+                cont_sect2_3_gc++;
+                h1_gc[0].textContent="Vittoria O";
+                h1_gc[0].style.color="red";
+                sect2_3_gc.textContent="Giocatore 2(O):"+ cont_sect2_3_gc;
+                n_cpu++;
                 return;
-            }    
+            }
+            if(
+                tasto_cpu[0].disabled && tasto_cpu[1].disabled && tasto_cpu[2].disabled &&
+                tasto_cpu[3].disabled && tasto_cpu[4].disabled && tasto_cpu[5].disabled &&
+                tasto_cpu[6].disabled && tasto_cpu[7].disabled && tasto_cpu[8].disabled
+            )
+            {
+                if(!vittoria_x_cpu && !vittoria_o_cpu)
+                {
+                    h1_gc[0].textContent = "Pareggio";
+                    h1_gc[0].style.color = "black";
+
+                    cont_sect2_2_gc++;
+                    sect2_2_gc.textContent = "Pareggi: " + cont_sect2_2_gc;
+
+                    return;
+                }
+            }
         });
     }
+
+    reset_gc.addEventListener("click",ResetPartita_gc);
+    
+    home_gc.addEventListener("click",function(){
+        gioco_2.style.display = "none";
+        titolo_gc.style.display = "none";
+        menù.style.display = "block";
+    });
 });
